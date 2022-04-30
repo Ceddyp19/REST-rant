0e6e6374e9181ee039656819e0b89c3bdb445ccb
Ceddyp19 <ceddyp117@gmail.com>
2022-04-29 20:46:56 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+/*
+import our router
+The first argument to app.use, /places sets all routes in the places controller relative to /places. 
+This means that /places will be added in front of any other path we define in the controller.
+*/
+app.use('/places', require('./controllers/places'))
+
 //if user requests non-existent page
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
