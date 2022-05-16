6dbdc42ccba5a8bce42e93a20573942ea5531d9d
Ceddyp19 <ceddyp117@gmail.com>
2022-05-16 17:58:20 -0500

    added validation to post route and custom error message on new page

--- controllers/places.js
+++ controllers/places.js
@@ -5,27 +5,36 @@ const db = require('../models')
 router.get('/', (req, res) => {
   //res.send('GET /places')
   db.Place.find()
-  .then(places => {
-    res.render('places/index', {places})
-  })
-  .catch(err => {
-    console.log(err)
-    res.render('error404')
-  })
+    .then(places => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
 })
 
 //POST
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-    res.redirect('/places')
-  })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
+    .then(() => {
+      res.redirect('/places')
+    })
+    .catch(err => {
+      if (err.name == "ValidationError") {
+        let message = 'Validation Error: '
+        // TODO: Genereate error message(s)
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+        res.render('places/new', { message })
+      } else {
+        console.log('err', err)
+        res.render('error404')
+      }
+    })
 })
-
 
 
 //NEW
@@ -43,7 +52,7 @@ router.get('/:id/edit', (req, res) => {
 
   } else {
     places.splice(id, 1)
-    res.render('places/edit', {place: places[id]})
+    res.render('places/edit', { place: places[id] })
   }
 })
 
@@ -65,12 +74,12 @@ router.delete('/:id', (req, res) => {
 //SHOW 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place => {
-    res.render('places/show', {place})
-  }).catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
+    .then(place => {
+      res.render('places/show', { place })
+    }).catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
 
 
 })
@@ -79,27 +88,27 @@ router.get('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-      res.render('error404')
+    res.render('error404')
   }
   else if (!places[id]) {
-      res.render('error404')
+    res.render('error404')
   }
   else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
 
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
+    // Save the new data into places[id]
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
   }
 })
 
